44d978d886c4b779a819dffacab1d5c53779a50a
elialm7 <metall65eli@gmail.com>
2024-06-26 00:44:13 -0400

    folder for tests added

--- src/controllers/CitiesController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-
-export const createCity = async (req: Request, res: Response): Promise<Response>=>{
-     return res.status(200).send({
-         sended: req.body
-     });
-
-};
